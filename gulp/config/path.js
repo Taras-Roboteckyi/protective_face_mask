67b8bf31fa9e82299e9ab430dc6703a2c1c6759d
Taras-Roboteckyi <rtvr@ukr.net>
2024-07-05 23:18:37 +0300

    added gulp settings

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,16 +7,19 @@ const srcFolder = `./src`;
 
 export const path = {
   build: {
+    js: `${buildFolder}/js/`,
     css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
     files: `${buildFolder}/files/`,
   }, // Папка з результатом програми
   src: {
+    js: `${srcFolder}/js/app.js`,
     scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`, //** - Провіряємо всі вкладені файли в папці files. *.* - Провіряємо усі файли з любим розширенням
   },
   watch: {
+    js: `${srcFolder}/js/**/*.js`,
     scss: `${srcFolder}/scss/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
     files: `${srcFolder}/files/**/*.*`,
--- gulpfile.js
+++ gulpfile.js
@@ -20,16 +20,18 @@ import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
 
 //Спостережувальна функція за змінами в файлах
 function watcher() {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
-  gulp.watch(path.watch.scss, scss); //1 параметр - Вказуєм шлях за яким потрібно дивитися і другий параметр - дії які потрібно виконати
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js); //1 параметр - Вказуєм шлях за яким потрібно дивитися і другий параметр - дії які потрібно виконати
 }
 
 //Основні задачі
-const mainTasks = gulp.parallel(copy, html, scss);
+const mainTasks = gulp.parallel(copy, html, scss, js);
 
 //Побудова сценаріїв виконання задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
