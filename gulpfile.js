32614914fd8b1a4f84e7b132678a82dad856ac04
Taras-Roboteckyi <rtvr@ukr.net>
2024-07-22 22:50:22 +0300

    changed npm version gulp, gulp-imagemin and browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 //Основний модуль
-import gulp from "gulp";
+//import gulp from "gulp";
 
 //Імпорт шляхів
-import { path } from "./gulp/config/path.js";
+//import { path } from "./gulp/config/path.js";
 
 //Імпорт загальних плагінів
-import { plugins } from "./gulp/config/plugins.js";
+//import { plugins } from "./gulp/config/plugins.js";
 
 //Передаєм значення в глобальну змінну
-global.app = {
+/* global.app = {
   path: path,
   gulp: gulp,
   plugins: plugins,
-};
+}; */
 
 //Імпорт задач
-import { copy } from "./gulp/tasks/copy.js";
+/* import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { serve } from "./gulp/tasks/server.js";
@@ -23,15 +23,15 @@ import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
-
+ */
 //Спостережувальна функція за змінами в файлах
-function watcher() {
+/* function watcher() {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
   gulp.watch(path.watch.images, images); //1 параметр - Вказуєм шлях за яким потрібно дивитися і другий параметр - дії які потрібно виконати
-}
+} */
 
 //Послідовна обробка шрифтів
 /* const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle); */
@@ -50,6 +50,87 @@ function watcher() {
 
 // Экспорт задач
 //const build = gulp.series(gulp.parallel(minifyHtml, optimizeImages));
-const watch = gulp.parallel(watcher, serve);
+/* const watch = gulp.parallel(watcher, serve);
 
-export default gulp.series(/* build, */ watch);
+export default gulp.series(build, watch);
+ */
+
+import gulp from "gulp";
+import htmlmin from "gulp-htmlmin";
+import imagemin from "gulp-imagemin";
+import mozjpeg from "imagemin-mozjpeg";
+import plumber from "gulp-plumber";
+import fileInclude from "gulp-file-include";
+import browserSync from "browser-sync";
+
+// Путь к исходным и выходным файлам
+const paths = {
+  html: {
+    src: "src/**/*.html",
+    dest: "dist/",
+  },
+  images: {
+    src: "src/images/**/*.{jpg,jpeg,png,svg,gif}",
+    dest: "dist/images/",
+  },
+};
+
+// Инициализация BrowserSync
+const server = browserSync.create();
+
+// Задача для минификации HTML
+function minifyHtml() {
+  return gulp
+    .src(paths.html.src)
+    .pipe(plumber()) // Предотвращение остановки на ошибке
+    .pipe(
+      fileInclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(server.stream());
+}
+
+// Задача для оптимизации изображений
+function optimizeImages() {
+  return gulp
+    .src(paths.images.src)
+    .pipe(plumber())
+    .pipe(
+      imagemin(/* [
+        mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ] */)
+    )
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(server.stream());
+}
+
+// Наблюдение за изменениями в файлах
+function watchFiles() {
+  gulp.watch(paths.html.src, minifyHtml);
+  gulp.watch(paths.images.src, optimizeImages);
+}
+
+// Задача для запуска сервера
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: "./dist",
+    },
+    port: 3000,
+  });
+  done();
+}
+
+// Экспорт задач
+const build = gulp.series(gulp.parallel(minifyHtml, optimizeImages));
+const watch = gulp.parallel(watchFiles, serve);
+
+export default gulp.series(build, watch);
